71485c8325026ceaee9409030c27f02943c33881
ZoieBoyd <zoieboyd@outlook.com>
2025-01-10 20:16:20 +0000

    Add functionality to delete button, read checkbox and modal form.
    - Added two new visual elements to the book cards; a delete button and a checkbox input element.
    - Styled the button and checkbox with CSS to give it a more visually appealing aesthetic that fits the rest of the webpage.
    - Utilised JavaScript to add functionality to the two new elements.
    - Utilised JavaScript to add functionality to the dialog/modal box. A book is now added to the display when it is submitted.

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const container = document.querySelector(".books-container");
 const newBookBtn = document.querySelector("#new-book-btn");
 const dialog = document.querySelector("dialog");
 const closeDialogBtn = document.querySelector("#close-btn");
-
-const myLibrary = [];
+const submitBtn = document.querySelector("#submit-btn");
+const form = document.querySelector("form");
 
 function Book(title, author, pages, hasRead) {
     this.title = title;
@@ -12,29 +12,63 @@ function Book(title, author, pages, hasRead) {
     this.hasRead = hasRead;
 }
 
+const myLibrary = [
+    new Book("The Hobbit", "J.R.R. Tolkien", 310, false),
+    new Book("Pride and Prejudice", "Jane Austen", 376, true),
+    new Book("To Kill a Mockingbird", "Harper Lee", 281, false),
+];
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function deleteBook(book) {
+    myLibrary.splice(myLibrary.indexOf(book), 1);
+    displayAllBooks();
+}
+
 function createBookCard(book) {
     const card = document.createElement("div");
-    card.classList.add("book-card");
     const content = document.createElement("div");
-    content.classList.add("book-card-content");
+    const titleContainer = document.createElement("div");
     const title = document.createElement("h1");
     const author = document.createElement("h2");
     const pages = document.createElement("p");
+    const readCheckbox = document.createElement("input");
+    const deleteBtn = document.createElement("button");
+
+    readCheckbox.type = "checkbox";
+    card.classList.add("book-card");
+    content.classList.add("book-card-content");
+    titleContainer.classList.add("title-container");
+    readCheckbox.classList.add("read-checkbox");
+    deleteBtn.classList.add("delete-btn");
+    
     title.textContent = book.title;
     author.textContent = book.author;
     pages.textContent = `${book.pages} pages`;
-    content.appendChild(title);
-    content.appendChild(author);
+    readCheckbox.checked = book.hasRead;
+
+    titleContainer.appendChild(title);
+    titleContainer.appendChild(author);
+    content.appendChild(titleContainer);
     content.appendChild(pages);
+    content.appendChild(readCheckbox);
+    content.appendChild(deleteBtn);
     card.appendChild(content);
     container.appendChild(card);
+
+    readCheckbox.addEventListener("click", () => {
+        book.hasRead = !book.hasRead;
+    });
+
+    deleteBtn.addEventListener("click", () => {
+        deleteBook(book);
+    });
 }
 
 function displayAllBooks() {
+    container.innerHTML = "";
     for(const book of myLibrary) {
         createBookCard(book);
     }
@@ -44,13 +78,26 @@ newBookBtn.addEventListener("click", () => {
     dialog.showModal();
 });
 
+submitBtn.addEventListener("click", (event) => {
+    const titleInput = document.getElementById("title-input").value;
+    const authorInput = document.getElementById("author-input").value;
+    const pagesInput = document.getElementById("pages-input").value;
+    const readInput = document.getElementById("read-input").checked;
+    
+    if (!titleInput || !authorInput || !pagesInput) {
+        alert("Please enter information for all of the fields.");
+        return;
+    }
+    
+    addBookToLibrary(new Book(titleInput, authorInput, pagesInput, readInput));
+    dialog.close();
+    form.reset();
+    displayAllBooks();
+})
+
 closeDialogBtn.addEventListener("click", () => {
     dialog.close();
+    form.reset();
 });
 
-addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 310, false));
-addBookToLibrary(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223, true));
-addBookToLibrary(new Book("Pride and Prejudice", "Jane Austen", 376, true))
-addBookToLibrary(new Book("To Kill a Mockingbird", "Harper Lee", 281, false));
-console.log(myLibrary);
 displayAllBooks();
